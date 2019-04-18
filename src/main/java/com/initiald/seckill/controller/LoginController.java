3dfa5df8a5d6f1fb014410f02ac94a9b4a468fd7
initiald0824 <initiald0824@163.com>
2019-04-18 22:01:22 +0800

    *

--- src/main/java/com/initiald/seckill/controller/LoginController.java
+++ src/main/java/com/initiald/seckill/controller/LoginController.java
@@ -1,9 +1,12 @@
 package com.initiald.seckill.controller;
 
 import com.initiald.seckill.redis.RedisService;
+import com.initiald.seckill.result.Result;
+import com.initiald.seckill.vo.LoginVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RequestMapping("/api")
 public class LoginController {
 
     @Autowired
@@ -19,5 +23,9 @@ public class LoginController {
 
     private static Logger log = LoggerFactory.getLogger(LoginController.class);
 
-
+    @RequestMapping("/login")
+    public Result login(LoginVo loginVo) {
+        log.info(loginVo.toString());
+        return null;
+    }
 }
---
+++ src/main/java/com/initiald/seckill/vo/LoginVo.java
@@ -0,0 +1,34 @@
+package com.initiald.seckill.vo;
+
+/**
+ * @author initiald0824
+ * @date 2019/4/18 11:51
+ */
+public class LoginVo {
+    private String mobile;
+    private String password;
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginVo{" +
+                "mobile='" + mobile + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
