ed4d51c27b0b32b2988664aa7f13f1b22c5129f2
initiald0824 <initiald0824@163.com>
2019-05-27 21:27:38 +0800

    *

---
+++ src/main/java/com/initiald/seckill/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.initiald.seckill.config;
+
+import com.initiald.seckill.config.interceptors.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author initiald0824
+ * @date 2019/5/27 19:12
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/login");
+    }
+}
---
+++ src/main/java/com/initiald/seckill/config/interceptors/LoginInterceptor.java
@@ -0,0 +1,19 @@
+package com.initiald.seckill.config.interceptors;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author initiald0824
+ * @date 2019/5/27 19:14
+ */
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+}
